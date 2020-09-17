97452a07bbbf07800511eb43454ed0e7b85f44b8
ruchikagohil22 <ruchikagohil22@gmail.com>
2020-09-17 22:23:11 +0530

    fix3

--- views/pages/index.js
+++ views/pages/index.js
@@ -3,6 +3,7 @@ const app=express()
 const User=require('./model/user')
 app.set('view engine','ejs')
 app.use(express.urlencoded())
+const bcrypt=require('bcryptjs')
 
 app.get('/',(req,res) =>{
     // res.send("hello world")
